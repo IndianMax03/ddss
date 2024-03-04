540e35291f181a6e4b12fec46c656d26e39dd9a3
IndianMax03 <vermamanu@mail.ru>
2024-03-04 03:26:24 +0300

    Update: script

--- lab-1/main.sql
+++ lab-1/main.sql
@@ -1,6 +1,6 @@
 DO $$
 DECLARE
-    t_name TEXT := 'link';
+    t_name TEXT := 'graph';
     s_name TEXT := 'public';
     no_header TEXT := 'No.';
     no_delimeter TEXT := '---';
@@ -11,21 +11,21 @@ DECLARE
     prev_attr TEXT := '';
     pointer CURSOR FOR (
         SELECT
-            pg_attribute.attnum,
-            pg_attribute.attname master_att,
-            pg_type.typname,
-            pg_constraint.contype, -- 'f', если fk --
-            pg_constraint.confrelid,
-            pg_clazz.relname,
-            pg_attribute2.attname slave_att
-        FROM pg_class 
-        JOIN pg_attribute ON pg_class.oid = pg_attribute.attrelid
-        JOIN pg_type ON pg_attribute.atttypid = pg_type.oid
-        JOIN pg_namespace ON pg_class.relnamespace = pg_namespace.oid
-        LEFT JOIN pg_constraint ON pg_attribute.attrelid = pg_constraint.conrelid AND pg_attribute.attnum = ANY(pg_constraint.conkey)
-        LEFT JOIN pg_attribute pg_attribute2 ON pg_constraint.confrelid = pg_attribute2.attrelid AND pg_constraint.confkey[1] = pg_attribute2.attnum
-        LEFT JOIN pg_class pg_clazz ON pg_clazz.oid = pg_attribute2.attrelid
-        where pg_class.relname = t_name and pg_namespace.nspname = s_name and pg_attribute.attnum > 0
+            patt.attnum num, -- номер аттрибута
+            patt.attname master_att, -- имя аттрибута
+            pt.typname, -- имя типа данных
+            pcstr.contype, -- 'f', если fk --
+            pcstr.confrelid, -- oid таблицы, на которую ссылается (если contype = 'f')
+            patt2.attname slave_att, -- имя аттрибута
+            pclass2.relname -- имя таблицы
+        FROM pg_class pclass -- все объекты, подобные таблицам
+        JOIN pg_attribute patt ON pclass.oid = patt.attrelid -- +аттрибуты, которые принадлежат таблицам
+        JOIN pg_type pt ON pt.oid = patt.atttypid -- +типы аттрибутов
+        JOIN pg_namespace pn ON pclass.relnamespace = pn.oid -- +пространство имен для таблицеподобной сущности
+        LEFT JOIN pg_constraint pcstr ON patt.attrelid = pcstr.conrelid AND patt.attnum = ANY(pcstr.conkey) -- +ограничения. LEFT чтобы оставить те, что без ограничений. ANY для исключения повторов
+        LEFT JOIN pg_attribute patt2 ON pcstr.confrelid = patt2.attrelid AND pcstr.confkey[1] = patt2.attnum -- +названия аттрибутов, на которые ссылаемся через fk
+        LEFT JOIN pg_class pclass2 ON pclass2.oid = patt2.attrelid -- +имя таблицы, на который ссылается наш fk
+        where pclass.relname = t_name and pn.nspname = s_name and patt.attnum > 0
     );
 BEGIN
 
@@ -49,12 +49,14 @@ BEGIN
             IF prev_attr <> '' THEN
                 RAISE NOTICE '% | % | %', no_delimeter, name_delimeter, attr_delimeter;
             END IF;
-            RAISE NOTICE '% | % | Type: %', RPAD(c.attnum::TEXT, 3, ' '), RPAD(c.master_att, 11, ' '), c.typname;
+            RAISE NOTICE '% | % | Type: %', RPAD(c.num::TEXT, 3, ' '), RPAD(c.master_att, 11, ' '), c.typname;
             IF c.contype = 'f' THEN
                 RAISE NOTICE '%|%| Constr: "%" References %(%)', RPAD('', 4, ' '),  RPAD('', 13, ' '), c.master_att, c.relname, c.slave_att;
             END IF;
         ELSE
-            RAISE NOTICE '%|%| Constr: "%" References %(%)', RPAD('', 4, ' '),  RPAD('', 13, ' '), c.master_att, c.relname, c.slave_att;
+            IF c.contype = 'f' THEN
+                RAISE NOTICE '%|%| Constr: "%" References %(%)', RPAD('', 4, ' '),  RPAD('', 13, ' '), c.master_att, c.relname, c.slave_att;
+            END IF;
         END IF;
         prev_attr = c.master_att;
     END LOOP;
